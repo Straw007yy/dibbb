4a642e8fbe96b410c89e02e1a9e38cb416184d6c
Glitch (hello-express) <none>
2020-06-24 20:15:47 +0000

    🎑🏁 Checkpoint ./watch.json:18085003/2 ./comandos/son.js:18085003/12803

--- comandos/son.js
+++ comandos/son.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (client, message, args) => {
-if (message.author.id !== '549373164464111618') return message.reply(":Alerta: | você não possui permissão para usar esse comando.");
+if (message.author.id !== '702675480154865685') return message.reply(":Alerta: | você não possui permissão para usar esse comando.");
 message.delete();
  
 let on = client.users.filter(m => m.presence.status === 'online')
@@ -17,27 +17,21 @@ let mensagem = args.join(" ")
   let fumante = client.users.get('631219646275780659')
       
 message.channel.send(`_**<a:carregandocdm:612824847831007232> A mensagem está sendo enviada para:**_\n\n` +
-`**<:botonline:725417730211053771> ${on.size}** onlines\n` +
+`**<:onlines:612823475035570179> ${on.size}** onlines\n` +
 `**<:ocupados:612823475471515658> ${npertube.size}** ocupados\n` +
 `**<:ausentes:612823475387891733> ${ausente.size}** ausentes\n\n` +
 `**<:discord:612823833979650079> Total de usuários:** ${todos.size}\n\n` +
 `__**<:offlines:612823401270214677> ${off.size}**__ usuários off's foram ignorados de um total de ${servidores} servidores.`)
 
-  on.forEach(f1 => {
-  f1.send(mensagem).catch(() => {})
-});
-
-npertube.forEach(f2 => {
-  f2.send(mensagem).catch(() => {})
-});
-
-ausente.forEach(f3 => {
-  f3.send(mensagem).catch(() => {})
-});
+on.forEach((f1) => {f1.send(mensagem)}); 
+    
+npertube.forEach((f2) => {f2.send(mensagem)});
+    
+ausente.forEach((f3) => {f3.send(mensagem)});
 
   fumante.forEach((f4) => {
         message.channel.send(`_**A mensagem foi enviada para:**_\n\n` +
-`**<:botonline:725417730211053771> ${on.size}** onlines\n` +
+`**<:onlines:612823475035570179> ${on.size}** onlines\n` +
 `**<:ocupados:612823475471515658> ${npertube.size}** ocupados\n` +
 `**<:ausentes:612823475387891733> ${ausente.size}** ausentes\n\n` +
 `**<:discord:612823833979650079> Total de usuários:** ${todos.size}\n\n` +
