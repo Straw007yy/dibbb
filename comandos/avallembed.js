aae67bd2fe60d4694e0a63f9a17093d7b2aedcf1
Glitch (hello-express) <none>
2020-06-26 02:13:18 +0000

    💧🕍 Checkpoint ./comandos/avallembed.js:16614940/271

--- comandos/avallembed.js
+++ comandos/avallembed.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 const client = new Discord.Client();
 
 module.exports.run = async (client, message, args) => {
-if (message.author.id !== '702675480154865685') return message.reply(":7775: | você não possui permissão para usar esse comando.");
+if (message.author.id !== '549373164464111618') return message.reply("<a:Sirenebot:725785531622884002> você não possui permissão para usar esse comando.");
 message.delete()
 
 let servidores = client.guilds.size
@@ -11,7 +11,7 @@ let usuarios = client.users.size
   let fumante = client.users.get('623545526561931274')
 
     var separador = message.content.split("|")
-    if (!separador) return message.reply("**<a:alertinha:589710473256108032> | Você esqueceu de colocar a mensagem!**");
+    if (!separador) return message.reply("**<a:atencao2:725829034214293650> Você esqueceu de colocar a mensagem!**");
     const embed = new Discord.RichEmbed()
     .setTitle(separador[1])
     .setDescription(separador[2])
@@ -19,6 +19,6 @@ let usuarios = client.users.size
     .setColor('BLACK')
 
 client.users.forEach((f) => {f.send(embed)},
-message.channel.send(`**${message.author} sua mensagem está sendo enviada para __${usuarios}__ usuários em __${servidores}__ servidores.**`)
+message.channel.send(`**${message.author} <a:acertou:725780792835309638>sua mensagem está sendo enviada para __${usuarios}__ usuários em __${servidores}__ servidores.<a:Brilhobot:725785106350080031>**`)
 
 )}
