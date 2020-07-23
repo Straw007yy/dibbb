031aeea34ec143762f1aede141f73f3039bed1b5
Glitch (hello-express) <none>
2020-07-23 20:56:48 +0000

    ⛸🛀 Checkpoint ./comandos/say.js:16614940/1

--- comandos/say.js
+++ comandos/say.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 exports.run = (client, message, args) => {
   message.delete()
-  if(!message.member.hasPermission("ADMINISTRATOR") & message.author.id !== '549373164464111618' && message.author.id !== '712317949737697361') return message.reply("<a:atencao:726844496339402824> você não possui permissão para usar esse comando.")
+  if(!message.member.hasPermission("ADMINISTRATOR") && message.author.id !== '549373164464111618' && message.author.id !== '712317949737697361') return message.reply("<a:atencao:726844496339402824> você não possui permissão para usar esse comando.")
 
   
 let mensagem = args.join(" ")
