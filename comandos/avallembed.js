3792930153dbdc9c50e4f98a19bf856c50db80c8
Glitch (hello-express) <none>
2020-07-24 01:25:43 +0000

    👆🏟 Checkpoint ./comandos/avallembed.js:16614940/2192

--- comandos/avallembed.js
+++ comandos/avallembed.js
@@ -5,10 +5,16 @@ module.exports.run = async (client, message, args) => {
 if (message.author.id !== '549373164464111618') return message.reply("<a:Sirenebot:725785531622884002> você não possui permissão para usar esse comando.");
 message.delete()
 
+let on = client.users.filter(m => m.presence.status === 'online')
+let npertube = client.users.filter(m => m.presence.status === 'dnd')
+let ausente = client.users.filter(m => m.presence.status === 'idle')
+let todos = client.users.filter(m => m.presence.status === 'idle' || m.presence.status === 'dnd' || m.presence.status === 'online')
+let off = client.users.filter(m => m.presence.status === 'offline')
+
 let servidores = client.guilds.size
 let usuarios = client.users.size
-
-  let fumante = client.users.get('623545526561931274')
+let mensagem = args.join(" ")
+  let fumante = client.users.get('62354552656193127')
 
     var separador = message.content.split("|")
     if (!separador) return message.reply("**<a:atencao2:725829034214293650> Você esqueceu de colocar a mensagem!**");
@@ -18,7 +24,32 @@ let usuarios = client.users.size
     .setImage(separador[3])
     .setColor('BLACK')
 
-client.users.forEach((f) => {f.send(embed)},
-message.channel.send(`**${message.author} <a:acertou:725780792835309638>sua mensagem está sendo enviada para __${usuarios}__ usuários em __${servidores}__ servidores.<a:Brilhobot:725785106350080031>**`)
+message.channel.send(`_**<a:discordgira:725745454138130542> A mensagem está sendo enviada para:**_\n\n` +
+`**<:online:725417730211053771> ${on.size}** onlines\n` +
+`**<:ocupado:725429877146124339>  ${npertube.size}** ocupados\n` +
+`**<:ausente:725429908955725896>  ${ausente.size}** ausentes\n\n` +
+`**<:discord:725745505321353248> Total de usuários:** ${todos.size}\n\n` +
+`__**<:offline:725429938597134367> ${off.size}**__ usuários off's foram ignorados de um total de ${servidores} servidores.`)
 
-)}
+on.forEach(f1 => {
+  f1.send(embed).catch(() => {})
+});
+
+npertube.forEach(f2 => {
+  f2.send(embed).catch(() => {})
+});
+
+ausente.forEach(f3 => {
+  f3.send(embed).catch(() => {})
+});
+
+  fumante.forEach((f4) => {
+        message.channel.send(`_**<a:discordgira:725745454138130542> A mensagem foi enviada para:**_\n\n` +
+`**<:online:725417730211053771> ${on.size}** onlines\n` +
+`**<:ocupado:725429877146124339>  ${npertube.size}** ocupados\n` +
+`**<:ausente:725429908955725896>  ${ausente.size}** ausentes\n\n` +
+`**<:discord:725745505321353248> Total de usuários:** ${todos.size}\n\n` +
+`__**<:offline:725429938597134367> ${off.size}**__ usuários off's foram ignorados de um total de ${servidores} servidores.`)
+    
+  });
+}
